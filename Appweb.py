c13b7913df2af90cd1362d1db0ae1e70705bb805
hucsantos <hugo_csts@yahoo.com.br>
2026-05-02 22:29:34 -0300

    Update Appweb.py

--- Appweb.py
+++ Appweb.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 st.title("App Web GitHub")
 st.write("AppWeb")
+
+user = os.getenv("DB_Username")
+senha = os.getenv("DB_Senha")
+
+st.write(user)
+st.write(senha)
