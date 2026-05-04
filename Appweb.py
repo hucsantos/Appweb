3284a73ef501e4a4aa1f815c8526fdb33ca81e09
hucsantos <hugo_csts@yahoo.com.br>
2026-05-03 23:04:24 -0300

    Update Appweb.py

--- Appweb.py
+++ Appweb.py
@@ -1,11 +1,5 @@
 import streamlit as st
-import pandas as pd
-import requests
-from io import BytesIO
-import openpyxl
-import plotly.express as px
-import os
-from dotenv import load_dotenv
+
 
 # Configuração da página
 st.set_page_config(page_title="Dashboard Backlog", layout="wide")
